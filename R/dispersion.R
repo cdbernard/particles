1fe8ee791d84ccf0e770a9458fbc45044871dcf1
cdbernard <56687398+cdbernard@users.noreply.github.com>
2022-08-10 20:39:22 +0100

    Update dispersion.R

--- R/dispersion.R
+++ R/dispersion.R
@@ -15,7 +15,7 @@ dispersion <- function(sample, limDataframe){
   for(i in 1:length(limDataframe)){
     binWidth <- limDataframe[[i]]$parameters[2]-limDataframe[[i]]$parameters[1]
     halfWidth <- binWidth/2
-    sampleDispersed[[i]] <- runif(particle_Number,
+    sampleDispersed[[i]] <- runif(particleNumber,
                                   sample[[i]]-halfWidth,
                                   sample[[i]]+halfWidth)
     names(sampleDispersed)[[i]] <- paste("param", i, sep = "")
